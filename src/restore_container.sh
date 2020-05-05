c830d421856ee69b04c88c4a14b9e41eedb6e6e4
Kaelan Fouwels <kaelan.fouwels@lagoni.co.uk>
2020-05-05 17:21:41 +0100

    iupdate helper script

--- src/restore_container.sh
+++ src/restore_container.sh
@@ -8,22 +8,29 @@ read -p "Press enter to confirm"
 
 echo ""
 echo "Going to drop and recreate 'alfresco' database. This may not exist."
+echo '/usr/bin/psql -h postgres -U alfresco -d postgres -c "drop database alfresco"'
+echo '/usr/bin/psql -h postgres -U alfresco -d postgres -c "create database alfresco"'
 read -p "Press enter to confirm"
 export PGPASSWORD="alfresco"
 /usr/bin/psql -h postgres -U alfresco -d postgres -c "drop database alfresco"
 /usr/bin/psql -h postgres -U alfresco -d postgres -c "create database alfresco"
 
 echo "Going to restore backup to 'alfresco' database."
+echo '/usr/bin/pg_restore -h postgres -U alfresco -d alfresco /tmp/db/alfresco.sql.Fc'
 read -p "Press enter to confirm"
 /usr/bin/pg_restore -h postgres -U alfresco -d alfresco /tmp/db/alfresco.sql.Fc
 
 echo ""
 echo "Going to remove and replace contentstore"
+echo 'rm -rf /opt/alfresco/alf_data/contentstore || true'
+echo 'cp -a /tmp/cs/contentstore /opt/alfresco/alf_data/contentstore'
 read -p "Press enter to confirm"
 rm -rf /opt/alfresco/alf_data/contentstore || true
 cp -a /tmp/cs/contentstore /opt/alfresco/alf_data/contentstore
 
 echo "Going to remove and replace contentstore.deleted. This may not exist"
+echo 'rm -rf /opt/alfresco/alf_data/contentstore.deleted || true'
+echo 'cp -a /tmp/cs/contentstore.deleted /opt/alfresco/alf_data/contentstore.deleted || true'
 read -p "Press enter to confirm"
 rm -rf /opt/alfresco/alf_data/contentstore.deleted || true
 cp -a /tmp/cs/contentstore.deleted /opt/alfresco/alf_data/contentstore.deleted || true
